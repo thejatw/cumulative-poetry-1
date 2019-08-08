8bc3258988cdd912a195ebd7992883a64105e063
Thejazeto.lhousa <thejazeto.lhousa@in-Thejazetolhousa.local>
2019-08-08 23:50:30 +0530

    Adds two test cases

--- src/main/java/CumulativePoetry.java
+++ src/main/java/CumulativePoetry.java
@@ -1,3 +1,34 @@
 public class CumulativePoetry {
+    private String[] lineForDay;
 
+    /**
+     * Constructor
+     */
+    public CumulativePoetry() {
+        populateLines();
+    }
+
+    /**
+     * Allocate memory for lines of each day and populate them with respective lines in reverse chronological order
+     */
+    private void populateLines() {
+        lineForDay = new String[12];
+
+        lineForDay[0] = "the horse and the hound and the horn that belonged to";
+        lineForDay[1] = "the farmer sowing his corn that kept";
+        lineForDay[2] = "the rooster that crowed in the morn that woke";
+        lineForDay[3] = "the priest all shaven and shorn that married";
+        lineForDay[4] = "the man all tattered and torn that kissed";
+        lineForDay[5] = "the maiden all forlorn that milked";
+        lineForDay[6] = "that cow with the crumpled horn that tossed";
+        lineForDay[7] = "the dog that worried";
+        lineForDay[8] = "the cat that killed";
+        lineForDay[9] = "the rat that ate";
+        lineForDay[10] = "the malth that lay in";
+        lineForDay[11] = "the house that Jack built.";
+    }
+
+    public String getLineForDay(int day) {
+        return "This is the house that Jack built.";
+    }
 }
--- src/test/java/CumulativePoetryTest.java
+++ src/test/java/CumulativePoetryTest.java
@@ -1,4 +1,24 @@
-public class CumulativePoetryTest {
-    private CumulativePoetry cumulativePoetry;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class CumulativePoetryTest {
+
+    @Test
+    void testCumulativePoetryForDayOne() {
+        CumulativePoetry cumulativePoetry = new CumulativePoetry();
+
+        String dayOne = "This is the house that Jack built.";
+        assertEquals(dayOne, cumulativePoetry.getLineForDay(1));
+    }
+
+    @Test
+    void testCumulativePoetryForDayTwo() {
+        CumulativePoetry cumulativePoetry = new CumulativePoetry();
+
+        String dayTwo = "This is the malt that lay in\n\tthe house that Jack built.";
+        assertEquals(dayTwo, cumulativePoetry.getLineForDay(2));
+    }
 
 }
